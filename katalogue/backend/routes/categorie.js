38959db8771f6f0e06fda3038a875161531fff0b
San Lamamba P <Sanlamamba05@gmail.com>
2022-03-13 18:29:52 +0000

    🐛 FIX: Images moving to wrong folder

--- katalogue/backend/routes/categorie.js
+++ katalogue/backend/routes/categorie.js
@@ -7,12 +7,12 @@ const categorieCtrl = require("../controllers/categorie");
 //multer COnfig
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./assets");
+    cb(null, "../public/assets/images/general/categories/");
   },
   filename: function (req, file, cb) {
     cb(
       null,
-      file.fieldname + "-" + Date.now() + "." + file.mimetype.split("/")[1]
+      file.originalname + "-" + Date.now() + "." + file.mimetype.split("/")[1]
     );
   },
 });
